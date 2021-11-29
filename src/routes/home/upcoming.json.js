13607218ab0ef8e7e08619c918df72eb15866d0f
Jedn <jed@nelson.ac>
2021-11-29 21:30:06 +0000

    Moved extra files to components directory

--- src/routes/home/upcoming.json.js
+++
@@ -1,6 +0,0 @@
-export function get() {
-  const url = 'http://www.api.hubdc.info/events/upcoming')
-  await fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
